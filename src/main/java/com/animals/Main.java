e6ba2a527e2c37253ef2774072c0e6e5e7f38d7f
Hubert <hubradw@gmail.com>
2019-06-21 18:41:19 +0200

    - Komentarze

--- src/main/java/com/animals/Main.java
+++ src/main/java/com/animals/Main.java
@@ -1,32 +1,31 @@
 package com.animals;
 
-import static java.lang.Math.min;
 import static java.lang.Math.random;
 
 public class Main {
 
     public static void main(String[] args) {
-        Cat cat = new Cat();
-        int minutesToEndOfDay = 840;
+        Cat cat = new Cat(); //TODO przydałby się drugi kot, a może trzy. Chociaż nie, lepiej 666 kotów
+        int minutesToEndOfDay = 840; // 60 * 24 czasami ułatwia życie a niewiele kosztuje TODO static final int
 
         while (minutesToEndOfDay > 0) {
-            int save = generateRandomNumber();
-            System.out.println("Czy kot jest wyspany? " + cat.isRested());
+            int save = generateRandomNumber(); //Przydałaby się lepsza nazna dla save
+            System.out.println("Czy kot jest wyspany? " + cat.isRested()); //Sprawdź sobie String.format("%s", "text")
             save = save > minutesToEndOfDay ? minutesToEndOfDay : save;
             if (cat.isRested()) {
-//                cat.makeVoice((int) random() * 10);
+//                cat.makeVoice((int) random() * 10); //TODO aktywuj to
                 cat.walkInMinutes(save);
-                System.out.println("Kot chodził: " + save);
+                System.out.println("Kot chodził: " + save);  //Sprawdź sobie String.format("%s", "text") TODO wypisz ile pozostało do końca dnia i podaj jednostki
             } else {
                 cat.sleep(save);
-                System.out.println("Kot spal: " + save);
+                System.out.println("Kot spal: " + save); //Sprawdź sobie String.format("%s", "text") TODO wypisz ile pozostało do końca dnia i podaj jednostki
             }
             minutesToEndOfDay = minutesToEndOfDay - save;
         }
         System.out.println("Czy kot jest wyspany? " + cat.isRested());
     }
 
-    static int generateRandomNumber() {
+    static int generateRandomNumber() { //TODO zrób prywatne
         return (int) (random() * 100);
     }
 }
