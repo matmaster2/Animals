7a7bef58fe80cc9347ede3be849778b1b29e55e4
Mateusz <matzarych@gmail.com>
2019-07-04 21:17:32 +0200

    - dodanie iteracji kota

--- src/main/java/com/animals/Main.java
+++ src/main/java/com/animals/Main.java
@@ -1,5 +1,8 @@
 package com.animals;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static java.lang.Math.random;
 
 public class Main {
@@ -7,29 +10,31 @@ public class Main {
     private static final int DAY_IN_MINUTES = 60 * 24;
 
     public static void main(String[] args) {
-        int minutesToEndOfDay = DAY_IN_MINUTES;
+        int i = 0;
+        List<Cat> CatsList = new ArrayList<>();
 
-        // List<Cat> CatsList = new ArrayList<Cat>();
-        Cat cat = new Cat(); //TODO przydałby się drugi kot, a może trzy. Chociaż nie, lepiej 666 kotów <----- spytac o szczegoly
-        // CatsList.add(cat);
-
-        while (minutesToEndOfDay > 0) {
-            int actionTime = generateRandomNumber();
-            System.out.format("Czy kot jest wyspany? %b\n", cat.isRested());
-            //System.out.println("Czy kot jest wyspany? " + cat.isRested()); //Sprawdź sobie String.format("%s", "text")
-            actionTime = actionTime > minutesToEndOfDay ? minutesToEndOfDay : actionTime;
-            if (cat.isRested()) {
-                cat.makeVoice(generateRandomNumber(10));
-                cat.walkInMinutes(actionTime);
-                System.out.format("Kot chodził %d godzin i %d minut\n", toHours(actionTime), actionTime % 60);
-            } else {
-                cat.sleep(actionTime);
-                System.out.format("Kot spał %d godzin i %d minut\n", toHours(actionTime), actionTime % 60);
+        do { //TODO czy 666 powinno byc tutaj? i dlaczego nie?  <-- spytac sie czemu nie powinoo byc wczesniej tam
+            int minutesToEndOfDay = DAY_IN_MINUTES;
+            CatsList.add(new Cat());
+            while (minutesToEndOfDay > 0) {
+                System.out.println("Dane na temat kota numer: "+i);
+                int actionTime = generateRandomNumber();
+                System.out.format("Czy kot jest wyspany? %b\n", CatsList.get(i).isRested());
+                actionTime = actionTime > minutesToEndOfDay ? minutesToEndOfDay : actionTime;
+                if (CatsList.get(i).isRested()) {
+                    CatsList.get(i).makeVoice(generateRandomNumber(10));
+                    CatsList.get(i).walkInMinutes(actionTime);
+                    System.out.format("Kot chodził %d godzin i %d minut\n", toHours(actionTime), actionTime % 60);
+                } else {
+                    CatsList.get(i).sleep(actionTime);
+                    System.out.format("Kot spał %d godzin i %d minut\n", toHours(actionTime), actionTime % 60);
+                }
+                System.out.format("Do końca dnia pozostało: %d godzin i %d minut\n", toHours(minutesToEndOfDay - actionTime), (minutesToEndOfDay - actionTime) % 60);
+                minutesToEndOfDay = minutesToEndOfDay - actionTime;
             }
-            System.out.format("Do końca dnia pozostało: %d godzin i %d minut\n", toHours(minutesToEndOfDay - actionTime), (minutesToEndOfDay - actionTime) % 60);
-            minutesToEndOfDay = minutesToEndOfDay - actionTime;
-        }
-        System.out.println("Czy kot jest wyspany? " + cat.isRested());
+            System.out.println("Czy kot jest wyspany? " + CatsList.get(i).isRested());
+            i++;
+        } while (i < 666);
     }
 
     private static int toHours(int minutes) {
