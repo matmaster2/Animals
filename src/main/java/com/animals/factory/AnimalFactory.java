4fa12d564f1a4efae5874e0ed9945be4e6f313e8
matmaster2 <matzarych@gmail.com>
2019-07-26 16:39:17 +0200

    - usprawnienie switcha

--- src/main/java/com/animals/factory/AnimalFactory.java
+++ src/main/java/com/animals/factory/AnimalFactory.java
@@ -8,10 +8,9 @@ public class AnimalFactory {
 
     private AnimalFactory(){}
 
-    public static Animal getAnimal(String animal){
-//todo jak usprawnic switcha? co mozna dac zamiast String animal?
-        //todo java 12 nowy switch-case
-        switch (animal){
+    public static Animal getAnimal(Animal animal){
+        //"todo java 12 nowy switch-case" - nie moge za bardzo instalowac javy 12 na moim komputerze w pracy, przez co wolalbym tego nie uzywac
+        switch (animal.toString()){
             case "cat": return new Cat("Azzura");
             case "dog": return new Dog("Ciapek");
             default: return null;
