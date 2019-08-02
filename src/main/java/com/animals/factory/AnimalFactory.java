c5e46914f849b3e5d77c654197a2636e78003835
matmaster2 <matzarych@gmail.com>
2019-08-02 17:01:16 +0200

    - zmiana switcha w AnimalFactory

--- src/main/java/com/animals/factory/AnimalFactory.java
+++ src/main/java/com/animals/factory/AnimalFactory.java
@@ -5,13 +5,15 @@ import com.animals.basic.Cat;
 import com.animals.basic.Dog;
 import com.animals.enums.TypeOfAnimal;
 
+import static com.animals.enums.TypeOfAnimal.*;
+
 public class AnimalFactory {
 
     private AnimalFactory(){}
 
-    public static Animal getAnimal(TypeOfAnimal animal){
+    public static Animal getAnimal(TypeOfAnimal typeOfAnimal){
 
-        switch (animal){
+        switch (typeOfAnimal){
             case SKINNY_CAT: return new Cat("Azzura");
             case CAT: return new Cat("Bartek");
             case FAT_CAT: return new Cat ("Hubert");
@@ -23,5 +25,17 @@ public class AnimalFactory {
 
 
 
+/*
+ public static Animal getAnimal(TypeOfAnimal typeOfAnimal){
 
-//"todo java 12 nowy switch-case"
+ Animal animal = switch (typeOfAnimal){
+ case SKINNY_CAT -> new Cat("Azzura");
+ case CAT -> new Cat("Bartek");
+ case FAT_CAT -> new Cat ("Hubert");
+ case DOG -> new Dog("Ciapek");
+ default -> {
+ break null;
+ }
+ };
+ return animal;
+ }*/
