218bac87eb514e38a2c499926567da02c5fc3698
Mateusz <matzarych@gmail.com>
2019-10-25 20:17:06 +0200

    - todo

---
+++ src/main/java/com/animals/async/AsyncSimulationOfTheDay.java
@@ -0,0 +1,16 @@
+package com.animals.async;
+
+public class AsyncSimulationOfTheDay implements Runnable {
+
+
+
+    @Override
+    public void run() {
+
+    }
+}
+
+
+//todo  dodac zwierze - leniwiec, panda
+// todo ktora baza sie nada tutaj
+// todo zrobic symulacje 1 (!) dnia zwierzat asynchronizczna (runnable, w tym samym czasie symulacje wszystkich zwierzat)
